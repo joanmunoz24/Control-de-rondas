35927b2460d2aff146ca23e357bd5223f24c1978
joan muñoz <joanmunoz24@gmail.com>
2023-03-15 11:40:48 -0300

    Initial Commit

---
+++ CurrentLocation/CurrentLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentLocationApp.swift
+//  CurrentLocation
+//
+//  Created by Joan Muñoz on 15-03-23.
+//
+
+import SwiftUI
+
+@main
+struct CurrentLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
